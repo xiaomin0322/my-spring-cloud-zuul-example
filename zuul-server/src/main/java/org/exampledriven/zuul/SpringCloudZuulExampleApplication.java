f0e30d23ea1d01e42283d04a418d1823f77f196d
zhangzengmin <xiaomin0322@sina.com>
2019-11-20 19:17:12 +0800

    update

--- zuul-server/src/main/java/org/exampledriven/zuul/SpringCloudZuulExampleApplication.java
+++ zuul-server/src/main/java/org/exampledriven/zuul/SpringCloudZuulExampleApplication.java
@@ -2,10 +2,12 @@ package org.exampledriven.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableCircuitBreaker
 public class SpringCloudZuulExampleApplication {
 
 	public static void main(String[] args) {
