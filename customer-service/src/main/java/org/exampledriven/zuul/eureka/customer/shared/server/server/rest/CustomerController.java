c91b18615c7cfe45a83963192047a4646642d14e
zhangzengmin <xiaomin0322@sina.com>
2020-01-08 16:28:43 +0800

    123

--- customer-service/src/main/java/org/exampledriven/zuul/eureka/customer/shared/server/server/rest/CustomerController.java
+++ customer-service/src/main/java/org/exampledriven/zuul/eureka/customer/shared/server/server/rest/CustomerController.java
@@ -1,10 +1,13 @@
 package org.exampledriven.zuul.eureka.customer.shared.server.server.rest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.exampledriven.zuul.eureka.customer.shared.server.server.domain.Customer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.LinkedList;
 import java.util.List;
@@ -12,21 +15,27 @@ import java.util.Optional;
 
 @RestController()
 public class CustomerController {
+	private static Logger logger_ = LogManager.getLogger(CustomerController.class);
+	private List<Customer> customers;
 
-    private List<Customer> customers;
+	public CustomerController() {
+		customers = new LinkedList<>();
+		customers.add(new Customer(1, "Peter", "Test"));
+		customers.add(new Customer(2, "Peter", "Test2"));
+	}
 
-    public CustomerController() {
-        customers = new LinkedList<>();
-        customers.add(new Customer(1, "Peter", "Test"));
-        customers.add(new Customer(2, "Peter", "Test2"));
-    }
+	@RequestMapping(value = "/customer/{id}", method = RequestMethod.GET, produces = "application/json")
+	public Customer getCustomer(@PathVariable int id) {
+		logger_.info("tracer.id {}",123);
+		Optional<Customer> customer = customers.stream().filter(customer1 -> customer1.getId() == id).findFirst();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return customer.get();
 
-    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET, produces = "application/json")     public Customer getCustomer(@PathVariable int id) {
-
-        Optional<Customer> customer = customers.stream().filter(customer1 -> customer1.getId() == id).findFirst();
-
-        return customer.get();
-
-    }
+	}
 
 }
